06b1942a32c5347fdd44eb416f32e3fb2839ca8e
Michael Wild <mwild95@live.co.uk>
2016-11-08 13:31:15 +0000

    Initial Commit

---
+++ Ordering/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ordering
+//
+//  Created by Michael Wild on 08/11/2016.
+//  Copyright © 2016 Bon Appetite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
